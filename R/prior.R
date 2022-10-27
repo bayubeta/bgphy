50f562375ae2ab2f6b74ed60bceb2b4356f47cb4
bayubeta <bayubrahmantio@gmail.com>
2022-10-27 14:09:49 +0200

    add print for priors

--- R/prior.R
+++ R/prior.R
@@ -12,6 +12,33 @@ prior.default <- function(model, ...){
 
 }
 
+#' @export
+print.prior <- function(priors){
+
+  varnames <- names(priors)
+
+  for (i in 1:length(varnames)){
+
+    # print variable name and its distribution
+    type <- attr(priors[[i]], "class")[1]
+    cat(varnames[i], " ~ ", type, "(", sep = "")
+
+    # print the parameter values
+    params <- attr(priors[[i]], "params")
+    parnames <- names(params)
+    for (j in 1:length(parnames)){
+      cat(parnames[j], " = ", params[[j]], sep = "")
+
+      if (j != length(parnames)){
+        cat(", ")
+      }
+      else{
+        cat(")")
+      }
+    }
+    cat("\n")
+  }
+}
 
 
 #=================== pdfs of prior distributions ===================
@@ -22,8 +49,9 @@ prior_uniform <- function(min = 0, max = 1){
     stats::dunif(x, min = min, max = max, log = TRUE)
   }
 
-  class(f) <- c("uniform", "prior")
+  class(f) <- c("uniform", "priorpdf")
   attr(f, "bounds") = c(min, max)
+  attr(f, "params") <- stats::setNames(c(min, max), c("min", "max"))
 
   return(f)
 }
